c5e833a72e83fd95a40b2371ccd728d7618fdd21
Stoian <stoian_ivanov_ivanov@abv.bg>
2016-01-15 15:05:50 +0200

    add the linkedlist tast

---
+++ SecondTask/src/main/Input.java
@@ -0,0 +1,6 @@
+package main;
+
+public interface Input {
+	public String read();
+
+}
---
+++ SecondTask/src/main/ScannerSPT1500.java
@@ -0,0 +1,8 @@
+package main;
+
+public class ScannerSPT1500 implements Scanner {
+    public String scan() {
+        return "SPT1500";
+    }
+}
+
